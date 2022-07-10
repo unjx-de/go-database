14daaa7aa42ec6e8035056bb16fd309e7dac7ac5
Florian Hoss <mail@florianhoss.de>
2022-07-10 11:45:01 +0200

    Initial commit

---
+++ database.go
@@ -0,0 +1,72 @@
+package database
+
+import (
+	"fmt"
+	"github.com/sirupsen/logrus"
+	"gorm.io/driver/mysql"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+	"net"
+	"time"
+)
+
+type MySQL struct {
+	Url      string
+	User     string
+	Password string
+	Database string
+	ORM      *gorm.DB
+}
+
+func (config *MySQL) tryDbConnection() {
+	i := 1
+	total := 20
+	for i <= total {
+		ln, err := net.DialTimeout("tcp", config.Url, 1*time.Second)
+		if err != nil {
+			if i == total {
+				logrus.WithField("attempt", i).Fatal("Failed connecting to database")
+			}
+			logrus.WithField("attempt", i).Warning("Connecting to database")
+			time.Sleep(2 * time.Second)
+			i++
+		} else {
+			_ = ln.Close()
+			logrus.WithField("attempt", i).Info("Connected to database")
+			i = total + 1
+		}
+	}
+}
+
+func (config *MySQL) initializeMySql() {
+	var err error
+	config.tryDbConnection()
+	dsn := fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
+		config.User,
+		config.Password,
+		config.Url,
+		config.Database,
+	)
+	config.ORM, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		logrus.WithField("error", err).Fatal("Failed to open database")
+	}
+}
+
+func (config *MySQL) initializeSqLite(storageDir string) {
+	var err error
+	absPath := storageDir + "config.sqlite"
+	config.ORM, err = gorm.Open(sqlite.Open(absPath), &gorm.Config{})
+	if err != nil {
+		logrus.WithField("error", err).Fatal("Failed to open database")
+	}
+}
+
+func (config *MySQL) Initialize(storageDir string) {
+	if config.Url == "" {
+		config.initializeSqLite(storageDir)
+	} else {
+		config.initializeMySql()
+	}
+	logrus.WithField("dialect", config.ORM.Dialector.Name()).Debug("Database initialized")
+}
---
+++ go.mod
@@ -0,0 +1,18 @@
+module github.com/unjx-de/go-database
+
+go 1.18
+
+require (
+	github.com/sirupsen/logrus v1.8.1
+	gorm.io/driver/mysql v1.3.4
+	gorm.io/driver/sqlite v1.3.6
+	gorm.io/gorm v1.23.8
+)
+
+require (
+	github.com/go-sql-driver/mysql v1.6.0 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
+	github.com/mattn/go-sqlite3 v1.14.12 // indirect
+	golang.org/x/sys v0.0.0-20191026070338-33540a1f6037 // indirect
+)
